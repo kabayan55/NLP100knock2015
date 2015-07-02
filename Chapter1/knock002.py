502eff069f45651e20e86d1a394bb4e80dfb82bc
kabayan55 <g1120513@is.ocha.ac.jp>
2015-07-02 13:48:09 +0900

    knock002

---
+++ Chapter1/knock002.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def mix_strs():
+    str1 = u"パトカー"
+    str2 = u"タクシー"
+    for char1,char2 in zip(str1,str2):
+        print str3 += str1 + str2
+if __name__ == "__main__":
+    mix_strs()
