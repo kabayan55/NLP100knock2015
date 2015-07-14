3fde0a01920aa3f2b1e2a505903d9b9713c623c4
kabayan55 <g1120513@is.ocha.ac.jp>
2015-07-15 00:04:30 +0900

    add knock005.py

---
+++ Chapter1/knock005.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+atomic_symbols= defaultdict(lambda:0)
+
+def word_bigram(words):
+
+
+
+def character_bigram(words):
+
+if __name__ == "__main__":
+    word_bigram()
+    literal_bigram()
