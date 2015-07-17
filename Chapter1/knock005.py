02059d43a73f8e8e698494260fcfe6068fee7f01
kabayan55 <g1120513@is.ocha.ac.jp>
2015-07-17 14:11:48 +0900

    edit knock005.py

--- Chapter1/knock005.py
+++ Chapter1/knock005.py
@@ -2,12 +2,25 @@
 from collections import defaultdict
 atomic_symbols= defaultdict(lambda:0)
 
-def word_bigram(words):
+def word_ngram(text,n):
+    text = text.translate(None,',.')
+    text_list = text.split(" ")
+    word_ngram_list=[]
+    if len(text_list) >= n:
+        for i in range(len(text_list)-n+1):
+            word_ngram_list.append(text_list[i:i+n])
+    return word_ngram_list
 
-
-
-def character_bigram(words):
+def character_ngram(text,n):
+    text = text.translate(None,' ,.')
+    character_ngram_list=[]
+    if len(text) >= n:
+        for i in range(len(text)-n+1):
+            character_ngram_list.append(text[i:i+n])
+    return character_ngram_list
 
 if __name__ == "__main__":
-    word_bigram()
-    literal_bigram()
+    text = "I am an NLPer"
+    n = 2
+    print word_ngram(text,n)
+    print character_ngram(text,n)
